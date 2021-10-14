b87be4938cd818de4183773c41ca70025135c389
Leonan MESCOITO <lmescoito@hotmail.com>
2021-10-13 23:45:09 -0300

    commit inicial

---
+++ temperatura.py
@@ -0,0 +1,44 @@
+temp = int(input('Entre com a temperatura: '))
+if temp < 0:
+print('congelando...')
+elif 0 <= temp <= 20:
+print('frio')
+elif 21 <= temp <= 25:
+print ('Normal')
+elif 26 <= temp <= 35:
+print ('Quente')
+else:
+print ('Muito quente!')
+
+# =============================================================================
+# =============================================================================
+# 
+# #1. Implementar duas funções:
+# #Uma que converta temperatura em graus Celsius para Fahrenheit.
+# #Outra que converta temperatura em graus Fahrenheit para Celsius. 
+# temp_C = int (input('Entre com a temperatura °C: '))
+# temp_F=0
+# for temp_F in range (temp_C+1):
+#     temp_F= ((9/5*temp_C)+32)
+# print('Temperatura em °F:{}'.format(temp_F))
+# 
+# =============================================================================
+#Escreva uma função que:
+#Receba uma frase como parâmetro.
+#Retorne uma nova frase com cada palavra com as letras invertidas.
+
+
+
+
+# =============================================================================
+# nome_inverso = nome
+# #if nome = nome_inverso:
+# for nome_inverso in range(nome):
+#     nome_inverso=(nome[::-1])
+#     print ('nome_inverso{}'.format(nome_inverso[::-1]))
+# #else:
+#     print(nome_inverso)
+#    
+# =============================================================================
+#print('Leonam')
+#print ('leonam'[::-1])
